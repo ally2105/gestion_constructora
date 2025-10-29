d0f0b49860640f153c245cf966aac4f631da2728
Allison Pauleth Segrera Pardo <allisonpauleth13@gmail.com>
2025-10-29 16:16:13 -0500

    Primer commit del proyecto

---
+++ gestion_construccion/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace gestion_construccion.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
